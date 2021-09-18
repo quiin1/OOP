f328024600c49f93aec156b621ec33921f696edb
quynhnt <trangquynh17@gmail.com>
2021-09-18 14:49:47 +0700

    Initial commit

---
+++ src/w1/Solution.java
@@ -0,0 +1,12 @@
+package w1;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        System.out.println(printHello());
+    }
+
+    public static String printHello() {
+        return "HelloWorld";
+    }
+}
